16c3543c54d35a18b3d6adc84a118f849f6e7f0a
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-05-02 20:33:11 -0500

    random image rekognition done

--- src/components/form.js
+++ src/components/form.js
@@ -14,9 +14,53 @@ import useStyles from '../styles/use-styles'
 import IconButton from '@material-ui/core/IconButton';
 import AutorenewIcon from '@material-ui/icons/Autorenew';
 import Tooltip from '@material-ui/core/Tooltip';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
+const {BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA, BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA }  = require ('../const/const')
+
+
+
+
+
+
 function Form(){
+
+    const[imageSelected, setImageSelected] = useState([]);
+    const[imageSelectedName, setImageSelectedName] = useState("");
+    const[textPrediction, setTextPrediction] = useState("");
+
+
+    
+    const onClicReload = () =>{
+      axios.get(BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA).then(response =>{
+        console.log(response.data.Name)
+        setImageSelected(response.data.Image)
+        setImageSelectedName(response.data.Name)
+      });
+    }
+    // https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition
+    const onClickPrediction = () =>{
+      axios.get(BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA+imageSelectedName).then(response =>{
+        console.log(response.data.text_predict_word)
+        setTextPrediction(response.data.text_predict_word);
+      });
+    }
+    let response ;
+    useEffect(() => {
+      axios.get(BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA).then(response =>{
+        console.log(response.data.Name)
+        setImageSelected(response.data.Image)
+        setImageSelectedName(response.data.Name)
+    
+      });
+   
+    },[]);
+
+
+  
+
+    
 
     const classes = useStyles();
     return(
@@ -31,47 +75,48 @@ function Form(){
         
         </Typography>
         <Grid className={classes.avatar} >
-          <Grid xs ={6}>
-              <img src="https://captcha.com/images/captcha/botdetect3-captcha-spiderweb.jpg"/>
+          <Grid item xs ={6}>
+              <img src= {`data:image/png;base64,${imageSelected}`} width="320" height="100"/>
           </Grid>
-          <Grid  xs ={6}>
+          <Grid  item xs ={6}>
           <Tooltip title="Reload captcha">
-              <IconButton aria-label="delete" className={classes.margin} size="small">
+              <IconButton aria-label="delete" className={classes.margin} size="small" onClick={onClicReload} >
                  <AutorenewIcon fontSize="inherit" />
             </IconButton>
             </Tooltip>
           </Grid>
         </Grid>
-       
+ 
         <form className={classes.form} noValidate>
        
           <TextField
             variant="outlined"
             margin="normal"
             disabled
-           
+            value= {textPrediction}
             fullWidth
             name="password"
-            label="Prediction"
-            type="password"
+           
+           
             id="password"
             autoComplete="current-password"
           />
         
           <Button
-            type="submit"
+
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
-          
+            onClick={onClickPrediction}
+
           >
             Rekognition
           </Button>
           <Grid container>
       
             <Grid >
-            <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
+            <input accept="image/*" className={classes.input} id="icon-button-file" type="file"/>
                 <label htmlFor="icon-button-file">
                 <Tooltip title="Upload captcha ">
                   <IconButton color="primary" aria-label="upload picture" component="span">
--- src/const/const.js
+++ src/const/const.js
@@ -1,1 +1,11 @@
-export const BASE_URL_GITHUB_FRONTEND_PROJECT = 'https://github.com/cristiancobo/aws-project-rekognition-frontend';
+const BASE_URL_GITHUB_FRONTEND_PROJECT = 'https://github.com/cristiancobo/aws-project-rekognition-frontend';
+const BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA = 'https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition/random';
+const BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA = 'https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition?name=';
+const BASE_URL_AWS_SERVICE_UPLOAD_CAPTCHA = 'https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition';
+
+module.exports = {
+    BASE_URL_GITHUB_FRONTEND_PROJECT,
+    BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA,
+    BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA ,
+    BASE_URL_AWS_SERVICE_UPLOAD_CAPTCHA
+}
