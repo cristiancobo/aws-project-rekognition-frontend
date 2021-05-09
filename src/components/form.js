f9089b3b7c4477d91c4b2b3ba6efa69ebb66960d
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-05-08 19:42:49 -0500

    project updated | feat: upload img done

--- src/components/form.js
+++ src/components/form.js
@@ -17,11 +17,8 @@ import Tooltip from '@material-ui/core/Tooltip';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import { render } from '@testing-library/react';
 const {BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA, BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA }  = require ('../const/const')
-
-
-
-
 
 
 function Form(){
@@ -29,30 +26,107 @@ function Form(){
     const[imageSelected, setImageSelected] = useState([]);
     const[imageSelectedName, setImageSelectedName] = useState("");
     const[textPrediction, setTextPrediction] = useState("");
+    const [selectedFile, setSelectedFile] = useState([]);
+    const [fileBase64String, setFileBase64String] = useState("");
+    const [nameFile, setNameFile] = useState("");
+    const[img, setImg] = useState("");
+    const [tamanio, setTamanio] = useState(0);
+    const [textPredict, setTexPredict] = useState("");
+    //const[data, setData] = useState({body:''})
 
 
     
     const onClicReload = () =>{
       axios.get(BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA).then(response =>{
         console.log(response.data.Name)
+        setTamanio(0)
         setImageSelected(response.data.Image)
         setImageSelectedName(response.data.Name)
+
       });
     }
-    // https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition
+
+    const onFileChange =(e)=>{
+      setSelectedFile(e.target.files);
+      
+      setNameFile(e.target.files[0].name)
+      setTamanio(1)
+    
+     
+
+    }
+
+    const encodeFileBase64 = (file) => {
+      var reader = new FileReader();
+      if (file) {
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          var Base64 = reader.result;
+          var convert = Base64.slice(23);
+          setImg(convert)
+          
+          setFileBase64String(Base64);
+         
+        };
+        reader.onerror = (error) => {
+          console.log("error: ", error);
+        };
+      }
+    };
+
+    encodeFileBase64(selectedFile[0])
+    
+    //https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition
     const onClickPrediction = () =>{
-      axios.get(BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA+imageSelectedName).then(response =>{
-        console.log(response.data.text_predict_word)
-        setTextPrediction(response.data.text_predict_word);
-      });
+
+      if(tamanio == 0){
+        axios.get(BASE_URL_AWS_SERVICE_REKOGNITION_CAPTCHA+imageSelectedName).then(response =>{
+          console.log(response.data.text_predict_word)
+          setTextPrediction(response.data.text_predict_word);
+        });
+      }else{
+
+            
+        const params = new URLSearchParams()
+        params.append('name', nameFile)
+        params.append('image',img)
+      
+        const config = {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+        
+        axios.post('https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition', params, config)
+          .then((result1) => {
+     
+            console.log(result1)
+            axios.get('https://w6re20r8ih.execute-api.us-east-1.amazonaws.com/dev/rekognition?name='+result1.data.Path)
+            .then((result2) => {
+       
+              console.log(result2)
+              setTexPredict(result2.data.text_predict_word);
+            })
+            .catch((err) => {
+              // Do somthing
+            })
+          })
+          .catch((err) => {
+            // Do somthing
+          })
+
+      }
+    
     }
     let response ;
     useEffect(() => {
       axios.get(BASE_URL_AWS_SERVICE_RANDOM_CAPTCHA).then(response =>{
-        console.log(response.data.Name)
+        //console.log(response.data.Name)
+        
+
         setImageSelected(response.data.Image)
         setImageSelectedName(response.data.Name)
-    
+      
       });
    
     },[]);
@@ -76,7 +150,8 @@ function Form(){
         </Typography>
         <Grid className={classes.avatar} >
           <Grid item xs ={6}>
-              <img src= {`data:image/png;base64,${imageSelected}`} width="320" height="100"/>
+             
+              <img src= {tamanio == 0 ? `data:image/png;base64,${imageSelected}`: fileBase64String }  width="320" height="100"/>
           </Grid>
           <Grid  item xs ={6}>
           <Tooltip title="Reload captcha">
@@ -93,7 +168,7 @@ function Form(){
             variant="outlined"
             margin="normal"
             disabled
-            value= {textPrediction}
+            value= {tamanio == 0 ? textPrediction : textPredict}
             fullWidth
             name="password"
            
@@ -116,7 +191,7 @@ function Form(){
           <Grid container>
       
             <Grid >
-            <input accept="image/*" className={classes.input} id="icon-button-file" type="file"/>
+            <input accept="image/*" className={classes.input} id="icon-button-file" type="file" onChange={ onFileChange}/>
                 <label htmlFor="icon-button-file">
                 <Tooltip title="Upload captcha ">
                   <IconButton color="primary" aria-label="upload picture" component="span">
